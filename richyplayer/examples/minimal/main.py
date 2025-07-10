8656a7fda32b404ba08edee34db5915d36d87785
richmondkleinson <richardkurniawan621@gmail.com>
2025-07-10 13:24:11 +0700

    fix demo & create minimal example

--- richyplayer/examples/minimal/main.py
+++ richyplayer/examples/minimal/main.py
@@ -11,12 +11,12 @@ import sys
 import pygame
 import asyncio
 
-
 import richyplayer
 
 
 def newPath(relPath: str) -> str:
-    relPath = relPath.replace(("/" if len(relPath.split("/")) > 1 else "\\"), os.sep)
+    relPath = relPath.replace(
+        ("/" if len(relPath.split("/")) > 1 else "\\"), os.sep)
     if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
         basePath = sys._MEIPASS  # type: ignore -> pyinstaller temp folder
     else:
@@ -27,25 +27,17 @@ def newPath(relPath: str) -> str:
 async def main():
     player = richyplayer.VideoPlayer()
     await player.open(
-        path=newPath("assets/video.mp4" if richyplayer.IS_WEB else "richyplayer/examples/demo/assets/video.mp4"),
+        path=newPath("assets/quaso.mp4" if richyplayer.IS_WEB else "richyplayer/examples/minimal/assets/quaso.mp4"),
         tmp_dir=newPath("/tmp/" if richyplayer.IS_WEB else "tmp/"),
         has_audio=True,
-        override_audio_source=newPath("assets/override.mp3" if richyplayer.IS_WEB else "richyplayer/examples/demo/assets/override.mp3"),
-    )
-
-    player2 = richyplayer.VideoPlayer()
-    await player2.open(
-        path="http://test-videos.co.uk/vids/bigbuckbunny/mp4/av1/360/Big_Buck_Bunny_360_10s_1MB.mp4",
-        tmp_dir=newPath("/tmp/" if richyplayer.IS_WEB else "tmp/"),
-        has_audio=False,
         override_audio_source=None,
     )
 
-    screen = pygame.display.set_mode((1000, 600))
+    screen = pygame.display.set_mode((player.width, player.height))
     clock = pygame.Clock()
 
     running = True
-    frameIndex = 0
+    frame_no = 0
 
     while running:
         for event in pygame.event.get():
@@ -55,23 +47,18 @@ async def main():
         screen.fill((0, 255, 0))
 
         if player.has_audio and not player.busy_audio():
-            player.play_audio(loops=1)
-        player.set_frame(frameIndex)
+            player.play_audio(loops=0)
+        player.set_frame(frame_no)
         screen.blit(player.get_frame(), (0, 0))
 
-        if player2.has_audio and not player2.busy_audio():
-            player2.play_audio()
-        player2.set_frame(frameIndex)
-        screen.blit(pygame.transform.scale_by(player2.get_frame(), 0.75), (500, 200))
-
-        frameIndex += 1
+        frame_no += 1
 
         pygame.display.flip()
-        clock.tick(30)
+        clock.tick(player.FPS)
         await asyncio.sleep(0)
 
     player.close()
-    player2.close()
     pygame.quit()
+
 
 asyncio.run(main())
